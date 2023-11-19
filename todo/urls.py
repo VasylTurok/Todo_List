c13fc95152cfcb94e6779393a72d4e254e0405bd
Vasyl Turok <turokvasil0@gmail.com>
2023-11-19 19:26:36 +0200

    add bottom change state, update task, delete task

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,31 @@
 from django.urls import path
-from todo.views import TaskListView, TaskCreateView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    change_the_ready_state,
+    TaskDeleteView,
+    TaskUpdateView,
+
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create")
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "task/<int:pk>/change_state/",
+        change_the_ready_state,
+        name="change_state",
+    ),
+    path(
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update",
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete",
+    ),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -16,3 +16,23 @@ class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("taxi:index")
+
+
+def change_the_ready_state(request, pk):
+    task = Task.objects.get(id=pk)
+    task.done = not task.done
+    print(task.done)
+    task.save()
+    next_url = request.GET.get('next', '/')
+    return redirect(next_url)
