93037a7ad7b1c4e9ae54ec4ec8e3266b675c36a6
Vasyl Turok <turokvasil0@gmail.com>
2023-11-19 16:05:21 +0200

    add static, temtlates, base.html, models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField()
+    deadline = models.DateTimeField()
+    done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.content
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    paginate_by = 10
+    template_name = "todo/index.html"
